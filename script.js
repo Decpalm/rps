e71ad6ff5b2ea511e9a71183bf3854614302bfc7
Decpalm <declanpalmateeer@gmail.com>
2025-06-03 21:26:40 -0400

    Updated script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello World")
+const hand = ['rock', 'paper', 'scissors']
+
+function getComputerChoice(){
+    return hand[Math.floor(Math.random()*3)];
+}
+
+function getHumanChoice(){
+    return prompt("Rock, Paper, or Scissors?").toLowerCase();
+}
